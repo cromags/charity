29d03eaddcd11c26fdc0c865c148e3114ea0079b
cromags <hcpunq@wp.pl>
2020-10-09 14:33:47 +0100

    number of bags + number of donations on homepage

--- src/main/java/pl/bw/charity/controllers/HomeController.java
+++ src/main/java/pl/bw/charity/controllers/HomeController.java
@@ -3,6 +3,7 @@ package pl.bw.charity.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.bw.charity.service.DonationDetailsService;
 import pl.bw.charity.service.OrganizationService;
 
 
@@ -10,15 +11,20 @@ import pl.bw.charity.service.OrganizationService;
 public class HomeController {
 
     private final OrganizationService organizationService;
+    private final DonationDetailsService donationDetailsService;
 
-    public HomeController(OrganizationService organizationService) {
+    public HomeController(OrganizationService organizationService,
+                          DonationDetailsService donationDetailsService) {
         this.organizationService = organizationService;
+        this.donationDetailsService = donationDetailsService;
     }
 
     @RequestMapping({"/", "/index"})
     public String homeAction(Model model){
 
         model.addAttribute("orgs", organizationService.findAll());
+        model.addAttribute("bags", donationDetailsService.getNumberOfBags());
+        model.addAttribute("donations", donationDetailsService.getNumberOfDonations());
         return "index";
     }
 }
---
+++ src/main/java/pl/bw/charity/service/DonationDetailsService.java
@@ -0,0 +1,28 @@
+package pl.bw.charity.service;
+
+import org.springframework.stereotype.Service;
+import pl.bw.charity.domain.model.DonationDetails;
+import pl.bw.charity.domain.repository.DetailsRepository;
+import pl.bw.charity.domain.repository.DonationRepository;
+
+@Service
+public class DonationDetailsService {
+
+    private final DetailsRepository detailsRepository;
+
+    public DonationDetailsService(DetailsRepository detailsRepository) {
+        this.detailsRepository = detailsRepository;
+    }
+
+    public Integer getNumberOfBags(){
+        Integer bags = 0;
+        for(DonationDetails d: detailsRepository.findAll()){
+            bags += d.getQuantity();
+        }
+        return bags;
+    }
+
+    public Integer getNumberOfDonations(){
+        return detailsRepository.findAll().size();
+    }
+}
