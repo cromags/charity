c7de3a8fd7668d0816ea0433be0c22a8cbc0cabc
cromags <hcpunq@wp.pl>
2020-10-16 16:26:55 +0100

    adding new org - admin feature

--- src/main/java/pl/bw/charity/controllers/AdminController.java
+++ src/main/java/pl/bw/charity/controllers/AdminController.java
@@ -2,11 +2,14 @@ package pl.bw.charity.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import pl.bw.charity.domain.model.Organization;
 import pl.bw.charity.domain.repository.OrganizationRepository;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -35,5 +38,21 @@ public class AdminController {
         }
         organizationRepository.delete(org);
         return "redirect:/";
+    }
+
+    @RequestMapping(value = "/addOrg")
+    public String addOrg(Model model) {
+        model.addAttribute("org", new Organization());
+        return "admin/addOrg";
+    }
+
+    @RequestMapping(value = "/doAddOrg", method = RequestMethod.POST)
+    public String doAddOrg(@Valid Organization organization, BindingResult bindingResult) {
+        System.out.println("jestem tu!!");
+        if (bindingResult.hasErrors()) {
+            return "redirect:/addOrg";
+        }
+        organizationRepository.save(organization);
+        return "redirect:/index";
     }
 }
